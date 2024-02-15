8e7ccc25b766a3ee24890b48627ddcc32639df4b
Mateusz Marter <mateuszmarter@gmail.com>
2024-02-15 17:25:25 +0100

    RunInConsole(compl)

--- RunApp.py
+++ RunApp.py
@@ -1,31 +1,35 @@
 import BankAccount
 
-
-'''
-class BankAccounts:
-    def __init__(self, name, amount):
-        self.name = name
-        self.amount = amount
-
-
-class Expenses:
-    def __init__(self, expence, cathegory):
-        self.expense = expence
-        self.cathegory = cathegory
-
-
-class Incomes:
-    def __init__(self, income, cathegory):
-        self.income = income
-        self.cathegory = cathegory
-BankAccount.BankAccount.func(12)
-'''
 run = True
 
-while run:
-    print("Dodaj przychod - 1\n"
-          "Dodaj wydatek - 2\n"
-          "Wyswietl stan konta - 3\n"
-          "Dodaj konto - 4\n"
-          "Zakoncz - 5\n")
+
+def stop():
+
+    global run
     run = False
+
+
+def choice(x):
+    action = {
+        '1': BankAccount.BankAccount.addIncome,
+        '2': BankAccount.BankAccount.addExpense,
+        '3': BankAccount.BankAccount.showBalance,
+        '4': BankAccount.BankAccount.add,
+        '5': stop
+    }
+
+    if x in action:
+        action[x]()
+    else:
+        print("Invalid value err")
+
+
+while run:
+    print("/////////////////////////////////")
+    userInput = input("Dodaj przychod - 1\n"
+                      "Dodaj wydatek - 2\n"
+                      "Wyswietl stan konta - 3\n"
+                      "Dodaj konto - 4\n"
+                      "Zakoncz - 5\n")
+
+    choice(userInput)
